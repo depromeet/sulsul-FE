8e9f37bfeebd8e82497400b2291a9b519efc7f92
cyjo9603 <cyjo9603@naver.com>
2022-04-21 23:08:21 +0900

    chore: 🔧 Pretendard font 적용

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- src/themes/GlobalStyle.tsx
+++ src/themes/GlobalStyle.tsx
@@ -1,12 +1,17 @@
 import { Global, css } from '@emotion/react';
 import emotionReset from 'emotion-reset';
 
-const GlobalStyle = () => (
-  <Global
-    styles={css`
-      ${emotionReset}
-    `}
-  />
-);
+const global = css`
+  ${emotionReset}
+
+  body {
+    // https://github.com/orioncactus/pretendard
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue',
+      'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  }
+`;
+
+const GlobalStyle = () => <Global styles={global} />;
 
 export default GlobalStyle;
