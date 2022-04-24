2bcbe77c5315e837dd33058a8da357305393f493
asst211118 <asst211118@tosspartners.com>
2022-04-24 18:18:15 +0900

    feat: svg 쓰기 위해 찬영님꺼 복붙

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
