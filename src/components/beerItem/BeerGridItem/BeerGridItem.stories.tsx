6936983793127f3bcf95141e8016e71954cee962
asst211118 <asst211118@tosspartners.com>
2022-04-24 18:23:47 +0900

    feat: :sparkles: BeerListIem, BeerGridItem 추가

---
+++ src/components/beerItem/BeerGridItem/BeerGridItem.stories.tsx
@@ -0,0 +1,10 @@
+import BeerGridItem from './BeerGridItem';
+
+export default {
+  title: 'Components/BeerGridItem',
+  component: 'BeerGridItem',
+};
+
+export const DefaultBeerGridItem = () => {
+  return <BeerGridItem />;
+};
---
+++ src/components/beerItem/BeerListItem/BeerListItem.stories.tsx
@@ -0,0 +1,10 @@
+import BeerListItem from './BeerListItem';
+
+export default {
+  title: 'Components/BeerListItem',
+  component: 'BeerListItem',
+};
+
+export const DefaultBeerListItem = () => {
+  return <BeerListItem />;
+};
