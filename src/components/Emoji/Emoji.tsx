28d114ae58e864f0f182a9c9926de308dcdf9631
asst211118 <asst211118@tosspartners.com>
2022-05-01 21:05:46 +0900

    refactor: :recycle: feel null 값 받을 수 있게 수정, alcohol 소수점 첫째자리까지 받도록 수정 등

--- src/components/Emoji/Emoji.tsx
+++ src/components/Emoji/Emoji.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import { getEmoji } from '@/utils';
 
 interface EmojiProps {
-  feel?: number;
+  feel: number | null;
 }
 
 const Emoji = ({ feel }: EmojiProps) => {
@@ -12,11 +12,12 @@ const Emoji = ({ feel }: EmojiProps) => {
 
 export default Emoji;
 
-const StyledEmoji = styled.div<{ feel?: number }>`
+const StyledEmoji = styled.div<{ feel: number | null }>`
   width: 2.7rem;
   height: 2.7rem;
   border-radius: ${({ feel }) => !feel && '50%'};
   background: ${({ feel }) =>
     !feel &&
     'linear-gradient(180deg, rgba(255, 255, 255, 0.3) 6.25%, rgba(255, 255, 255, 0.1) 87.5%, rgba(255, 255, 255, 0) 87.5%)'};
+  //FIXME: 원이 잘려서 나옴...
 `;
