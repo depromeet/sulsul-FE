2b4c727428f202c596c8ba2c8394bfcb36e89dfb
asst211118 <asst211118@tosspartners.com>
2022-05-06 22:38:55 +0900

    feat: :sparkles: BeerList 컴포넌트 추가, BeerImageMasking 컴포넌트 추가

---
+++ src/components/commons/BeerImageMasking/BeerImageMasking.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { ReactNode } from 'react';
+import type { PropsWithChildren } from 'react';
+
+interface Props {
+  width?: string;
+  children?: ReactNode;
+}
+
+const BeerImageMasking = (props: PropsWithChildren<Props>) => {
+  const { width, children, ...rest } = props;
+  return (
+    <StyledBeerImageMasking width={width} {...rest}>
+      {children}
+    </StyledBeerImageMasking>
+  );
+};
+
+export default BeerImageMasking;
+
+// NOTE: 마스킹 참고 : https://www.w3schools.com/css/css3_masking.asp
+const StyledBeerImageMasking = styled.div<Props>`
+  width: ${(p) => p.width};
+  aspect-ratio: 2 / 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  -webkit-mask-box-image: url('https://ifh.cc/g/KQ8NLv.png');
+  mask-image: url('https://ifh.cc/g/KQ8NLv.png');
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+`;
