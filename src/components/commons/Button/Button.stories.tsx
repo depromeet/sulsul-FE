179ca8a21362a74e2083e7c5a5bc9bde03b6b7b2
cyjo9603 <cyjo9603@naver.com>
2022-05-14 16:26:24 +0900

    refactor: ♻️ storybook Icon 컴포넌트 변경

--- src/components/commons/Button/Button.stories.tsx
+++ src/components/commons/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { FlightTakeOffIcon } from '@/assets/icon';
+import Icon from '@/components/commons/Icon';
 
 import Button from './Button';
 
@@ -29,8 +29,8 @@ export default {
 const Template: ComponentStory<typeof Button> = ({ leftAddon, rightAddon, ...args }) => (
   <Button
     {...args}
-    leftAddon={leftAddon ? <FlightTakeOffIcon /> : undefined}
-    rightAddon={rightAddon ? <FlightTakeOffIcon /> : undefined}
+    leftAddon={leftAddon ? <Icon name="FlightTakeOff" /> : undefined}
+    rightAddon={rightAddon ? <Icon name="FlightTakeOff" /> : undefined}
   />
 );
 
