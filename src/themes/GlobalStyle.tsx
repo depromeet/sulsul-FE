30dcf3893354d69a038b97889dea4a0d4558c945
syoung125 <laura120555@gmail.com>
2022-05-17 01:51:43 +0900

    chore: :lipstick: body 태그 배경 검은색으로 설정

--- src/themes/GlobalStyle.tsx
+++ src/themes/GlobalStyle.tsx
@@ -86,6 +86,7 @@ const global = css`
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue',
       'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
       'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+    background-color: #000000;
   }
 `;
 
