34e27e90a00a8e1cf41e268decc90b9d6bc67fe7
syoung125 <laura120555@gmail.com>
2022-05-22 01:34:12 +0900

    feat: :sparkles: 맥주 정렬 선택 바텀싵 추가

---
+++ src/components/BeerListSortBottomSheet/BeerListSortBottomSheet.tsx
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled';
+
+import BottomSheet from '../BottomSheet';
+import Icon from '../commons/Icon';
+
+import { BeerListSortType, beerListSortTextAlias } from '@/types/Beer';
+import { theme } from '@/themes';
+
+interface BeerListSortBottomSheetProps {
+  sortType: BeerListSortType;
+  open: boolean;
+  onClose: VoidFunction;
+  onItemClick: (sortType: BeerListSortType) => void;
+}
+
+const StyledWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 25px;
+`;
+
+const SortTypeItem = styled.li<{ isSelected: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+  p {
+    ${(p) => p.theme.fonts.SubTitle2};
+    color: ${(p) => (p.isSelected ? p.theme.color.yellow : p.theme.color.whiteOpacity50)};
+    padding: 0 10px;
+    margin-left: 30px;
+  }
+
+  + li {
+    margin-top: 25px;
+  }
+`;
+
+const BeerListSortBottomSheet = ({
+  sortType: currentSortType,
+  open,
+  onClose,
+  onItemClick,
+}: BeerListSortBottomSheetProps) => {
+  return (
+    <BottomSheet open={open} onClose={onClose}>
+      <StyledWrapper>
+        {Object.entries(beerListSortTextAlias).map(([sortType, sortText]) => {
+          const isSelected = sortType === currentSortType;
+          return (
+            <SortTypeItem
+              key={sortType}
+              aria-checked={isSelected}
+              isSelected={isSelected}
+              onClick={() => onItemClick(sortType as BeerListSortType)}
+            >
+              <p>{sortText}</p>
+              <Icon
+                name="Check"
+                size={30}
+                color={isSelected ? theme.color.yellow : 'transparent'}
+                aria-hidden={true}
+              />
+            </SortTypeItem>
+          );
+        })}
+      </StyledWrapper>
+    </BottomSheet>
+  );
+};
+
+export default BeerListSortBottomSheet;
--- src/types/Beer.ts
+++ src/types/Beer.ts
@@ -22,3 +22,12 @@ export interface Beer {
   feel?: number | null;
   isLiked?: boolean;
 }
+
+export type BeerListSortType = 'ALCOHOL_ASC' | 'ALCOHOL_DESC' | 'NAME' | 'REVIEW';
+
+export const beerListSortTextAlias: Record<BeerListSortType, string> = {
+  NAME: '맥주 이름 순',
+  REVIEW: '리뷰 많은 순',
+  ALCOHOL_DESC: '높은 도수 순',
+  ALCOHOL_ASC: '낮은 도수 순',
+};
