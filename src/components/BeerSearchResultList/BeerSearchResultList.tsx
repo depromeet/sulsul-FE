eae3055abbd051b6751c7d5ca6808afacb8baddf
syoung125 <laura120555@gmail.com>
2022-05-27 09:13:41 +0900

    feat: :sparkles: BeerSearchResultList 추가

---
+++ src/components/BeerSearchResultList/BeerSearchResultList.tsx
@@ -0,0 +1,62 @@
+import { useQuery } from 'react-query';
+import { useRecoilValue } from 'recoil';
+import { useRouter } from 'next/router';
+import { isNil } from 'lodash';
+
+import BeerList from '../BeerList';
+import BeerSearchResultEmpty from '../BeerSearchResultEmpty';
+
+import {
+  $beerListFilter,
+  $beerListSortBy,
+  $beerListViewType,
+} from '@/containers/BeerListContainer/atoms';
+import { getBeers } from '@/apis';
+
+const BeerSearchResultList = () => {
+  const router = useRouter();
+  const query = isNil(router.query.query) ? undefined : String(router.query.query);
+
+  const listViewType = useRecoilValue($beerListViewType);
+  const filter = useRecoilValue($beerListFilter);
+  const sortBy = useRecoilValue($beerListSortBy);
+
+  const payload = {
+    query,
+    filter,
+    sortBy: [sortBy],
+  };
+
+  const { data } = useQuery(
+    ['beers', payload],
+    async () =>
+      await getBeers({
+        ...payload,
+        cursor: 0,
+        limit: 20,
+      }),
+  );
+
+  const beers = data?.data.contents;
+
+  if (!beers) {
+    return null;
+  }
+
+  if (!beers.length) {
+    return (
+      <BeerSearchResultEmpty
+        title={query ? `“${query}”\n맥주를 찾을 수 없어요.` : `해당 맥주가 없어요.`}
+        subtitle={
+          query
+            ? `잘못된 검색어를 입력하였거나,\n등록되지 않은 맥주예요.`
+            : '검색 조건을 변경해보세요.'
+        }
+      />
+    );
+  }
+
+  return <BeerList type={listViewType} beers={beers} />;
+};
+
+export default BeerSearchResultList;
