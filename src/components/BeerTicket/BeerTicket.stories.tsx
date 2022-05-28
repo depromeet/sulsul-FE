0ee9b4fd3165759a3da8387a1e91323a005cb2dc
cyjo9603 <cyjo9603@naver.com>
2022-05-29 01:56:33 +0900

    feat: ✨BeerTicketStamp 컴포넌트 타입 분기

--- src/components/BeerTicket/BeerTicket.stories.tsx
+++ src/components/BeerTicket/BeerTicket.stories.tsx
@@ -11,21 +11,38 @@ export default {
 
 const Template: ComponentStory<typeof BeerTicket> = (args) => <BeerTicket {...args} />;
 
+const beer = Beers[1];
+const record = {
+  prevCountryNameEng: 'deuee',
+  nextCountryNameEng: 'korea',
+  prevCountryNameKor: '독일',
+  nextCountryNameKor: '한국',
+  feel: 3,
+  flavors: [
+    { id: 1, content: '목넘김이 부드러워요' },
+    { id: 2, content: '목넘김이 부드러워요22' },
+    { id: 3, content: '목넘김이 부드러워요33' },
+  ],
+  content: 'test',
+  recodedAt: new Date(),
+};
+
 export const 티켓 = Template.bind({});
 티켓.args = {
-  beer: Beers[1],
-  record: {
-    prevCountryNameEng: 'deuee',
-    nextCountryNameEng: 'korea',
-    prevCountryNameKor: '독일',
-    nextCountryNameKor: '한국',
-    feel: 3,
-    flavors: [
-      { id: 1, content: '목넘김이 부드러워요' },
-      { id: 2, content: '목넘김이 부드러워요22' },
-      { id: 3, content: '목넘김이 부드러워요33' },
-    ],
-    content: 'test',
-    recodedAt: new Date(),
-  },
+  beer: beer,
+  record: record as any,
+};
+
+export const 티켓이랑도장 = Template.bind({});
+티켓이랑도장.args = {
+  beer: beer,
+  record: record as any,
+  type: 'stamp',
+};
+
+export const 티켓이랑도장2 = Template.bind({});
+티켓이랑도장2.args = {
+  beer: beer,
+  record: { ...record, feel: 5, recodedAt: new Date(2022, 1, 1) } as any,
+  type: 'stamp',
 };
