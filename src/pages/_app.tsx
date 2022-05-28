1d20bb2e6f5d030ff0138be8907cd76d4f9f796d
syoung125 <laura120555@gmail.com>
2022-05-29 03:24:39 +0900

    Revert "fix: :bug: Hydration failed 에러 해결"
    This reverts commit 3d415d603b063c4588b6e0c5af91dabe7a947378.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app';
@@ -9,16 +8,6 @@ import MainLayout from '@/components/MainLayout';
 import queryClient from '@/configs/queryClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [domLoaded, setDomLoaded] = useState(false);
-
-  useEffect(() => {
-    setDomLoaded(true);
-  }, []);
-
-  if (!domLoaded || typeof window === 'undefined') {
-    return null;
-  }
-
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
