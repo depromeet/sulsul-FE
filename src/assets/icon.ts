430cd35c6fa0d5bc17eecd621154fb0472edcdc6
Ko Seoyoung <laura120555@gmail.com>
2022-05-31 10:12:20 +0900

    feat: 맥주 요청 수하물 티켓 레이아웃 추가 (#78)
    * chore: :bento: 바코드 아이콘 추가
    
    * feat: :sparkles: 맥주 요청 수화물 티켓 레이아웃 추가
    
    * chore: 스토리북 네이밍 변경

--- src/assets/icon.ts
+++ src/assets/icon.ts
@@ -4,6 +4,7 @@ export { ReactComponent as Cloud } from './cloud.svg';
 export { ReactComponent as ConveyorBelt } from './conveyor_belt.svg';
 export { ReactComponent as History } from './history.svg';
 export { ReactComponent as Back } from './back.svg';
+export { ReactComponent as Barcode } from './barcode.svg';
 export { ReactComponent as Search } from './search.svg';
 export { ReactComponent as FlightTakeOff } from './flight_takeoff.svg';
 export { ReactComponent as Good } from './emoji_good.svg';
---
+++ src/components/layouts/BeerRequestLayout/BeerRequestLayout.tsx
@@ -0,0 +1,70 @@
+import { ReactChild } from 'react';
+import styled from '@emotion/styled';
+
+import Icon from '@/components/commons/Icon';
+
+interface BeerRequestLayoutProps {
+  title: string;
+  children: ReactChild;
+}
+
+const StyledBeerRequestLayout = styled.article`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  height: 520px;
+  padding: 70px 0 90px 0;
+  border-radius: 20px;
+
+  background-color: ${(p) => p.theme.color.white};
+
+  overflow: hidden;
+
+  > .barcode-top {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+  }
+
+  > .barcode-bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+  }
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  border-left: 12px solid ${(p) => p.theme.color.yellow};
+  border-right: 12px solid ${(p) => p.theme.color.yellow};
+
+  > h1 {
+    margin: 4px 0 0;
+    ${(p) => p.theme.fonts.H4};
+    color: ${(p) => p.theme.color.black100};
+    text-align: center;
+    white-space: pre-line;
+  }
+`;
+
+const BeerRequestLayout: React.FC<BeerRequestLayoutProps> = ({ title, children }) => {
+  return (
+    <StyledBeerRequestLayout>
+      <Icon className="barcode-top" name="Barcode" width="240px" height="24px" />
+      <StyledWrapper>
+        <h1>{title}</h1>
+        {children}
+      </StyledWrapper>
+      <Icon className="barcode-bottom" name="Barcode" width="240px" height="60px" />
+    </StyledBeerRequestLayout>
+  );
+};
+
+export default BeerRequestLayout;
