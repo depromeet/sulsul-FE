e8dce6e56c43f7f73d0ea05edff37063fd9f7e75
syoung125 <laura120555@gmail.com>
2022-06-02 03:07:57 +0900

    feat: QueryParams util 추가

--- src/utils/query-params.ts
+++ src/utils/query-params.ts
@@ -5,16 +5,19 @@ const getQueryParams = (key: string) => {
     get: (searchParams: URLSearchParams, prop: string) => searchParams.get(prop),
   });
 
-  return params[key] ? decodeURI(params[key]) : params[key];
+  return params[key];
 };
 
 /** @Note 새로고침을 방지하려면 replace prop으로 router.replace 함수를 넘긴다  */
-const setQueryParams = (key: string, value: any, replace?: (href: string) => void) => {
+const setQueryParams = (key: string, value: any, replace?: (href: URL) => void) => {
+  if (!value) {
+    return;
+  }
+
   const href = new URL(window.location.href);
+  href.searchParams.set(key, value);
 
-  href.searchParams.set(key, encodeURI(value));
-
-  replace ? replace(href.toString()) : window.location.replace(href.toString());
+  replace ? replace(href) : window.location.replace(href.toString());
 };
 
 const QueryParams = {
