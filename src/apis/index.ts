bfba60c0c0c531fe16ab5261e6a8e6abf40b623c
cyjo9603 <cyjo9603@naver.com>
2022-06-01 17:52:50 +0900

    feat: ✨api BaseResponse interface 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,3 +1,13 @@
 export * from './beer';
 export * from './continent';
 export * from './country';
+
+export interface IError {
+  message: string;
+  code: number;
+}
+export interface IBaseResponse<T extends any> {
+  success: boolean;
+  contents: T;
+  error?: IError | null;
+}
