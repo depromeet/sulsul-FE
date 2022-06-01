c682d376499dd7c8bcf0f91599dc030d27dce21b
Ko Seoyoung <laura120555@gmail.com>
2022-06-01 14:33:49 +0900

    feat: 맥주 요청 완료 페이지 추가 (#81)
    * feat: :sparkles: 맥주 요청 완료 페이지 추가
    
    * feat: '방금 등록한 맥주 기록하기' 버튼 제거

---
+++ src/pages/beer/request/complete.tsx
@@ -0,0 +1,7 @@
+import BeerRequestCompleteContainer from '@/containers/BeerRequestCompleteContainer';
+
+const BeerRequestCompletePage = () => {
+  return <BeerRequestCompleteContainer />;
+};
+
+export default BeerRequestCompletePage;
