e091ae6028f677807b4b550c7b34460b1ae7a470
syoung125 <laura120555@gmail.com>
2022-06-06 23:17:32 +0900

    refactor: :recycle: api url .env에서 불러오도록 수정

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,6 @@
 import axios, { Axios } from 'axios';
 
-/** @todo .env 파일에서 불러오기 */
-export const API_BASE_URL = 'http://ec2-3-34-48-218.ap-northeast-2.compute.amazonaws.com:8080/';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const axiosClient: Axios = axios.create({
   baseURL: API_BASE_URL,
