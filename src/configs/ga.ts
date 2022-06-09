17555c190546cc8594706e87493fd572f68e4a9f
syoung125 <laura120555@gmail.com>
2022-06-10 01:45:15 +0900

    chore: gaMeasurementId production 일때만 측정하도록 설정

--- src/configs/ga.ts
+++ src/configs/ga.ts
@@ -1,1 +1,2 @@
-export const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || '';
+export const gaMeasurementId: string =
+  (process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID : '') ?? '';
