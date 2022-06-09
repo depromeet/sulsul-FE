a4848cc82c4cdc2abfa57fce044dea6750b98347
cyjo9603 <cyjo9603@naver.com>
2022-06-10 01:38:21 +0900

    development 환경에서 api proxy server 설정

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,9 @@
 import axios, { Axios } from 'axios';
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
+
 const axiosClient: Axios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
+  baseURL: process.env.NODE_ENV === 'production' ? BASE_URL : '',
   timeout: 3000,
   withCredentials: true,
 });
