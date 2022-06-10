310373f943aac0b997bdfaee0898cf56e5fcdc92
cyjo9603 <cyjo9603@naver.com>
2022-06-10 22:05:18 +0900

    fix: 🐛axios server side에서는 proxy가 아닌 서버로 직접 요청

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,9 +1,10 @@
 import axios, { Axios } from 'axios';
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
+const isServer = typeof window === 'undefined';
 
 const axiosClient: Axios = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? BASE_URL : '',
+  baseURL: process.env.NODE_ENV === 'production' || isServer ? BASE_URL : '',
   timeout: 3000,
   withCredentials: true,
 });
