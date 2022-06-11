b38adaca35da571c60b40db06d1a7a7d7ecb0058
cyjo9603 <cyjo9603@naver.com>
2022-06-11 16:36:13 +0900

    feat: ✨ recordList 컴포넌트 구현

---
+++ src/components/RecordList/RecordList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import { IRecord } from '@/apis';
+import RecordListItem from '@/components/RecordListItem';
+
+interface RecordListProps {
+  records: IRecord[];
+}
+
+const StyledRecordList = styled.section`
+  & > .records-list-item {
+    margin-bottom: 36px;
+    position: relative;
+
+    &:not(:last-child)::before {
+      position: absolute;
+      content: ' ';
+      width: 0px;
+      height: 63px;
+      border: 1px dashed ${({ theme }) => theme.color.whiteOpacity80};
+      bottom: 15px;
+      left: 17px;
+      transform: translateY(100%);
+    }
+  }
+`;
+
+const RecordList: React.FC<RecordListProps> = ({ records }) => {
+  return (
+    <StyledRecordList>
+      {records.map((record) => (
+        <RecordListItem key={record.id} record={record} className="records-list-item" />
+      ))}
+    </StyledRecordList>
+  );
+};
+
+export default RecordList;
