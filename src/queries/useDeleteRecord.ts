57abbf54adfb7c23a40bc9699b76cb8bf44d0ffa
hy57in <asst211118@tosspartners.com>
2022-06-20 18:32:10 +0900

    feat: :children_crossing: 에러발생 시 alert 추가

--- src/queries/useDeleteRecord.ts
+++ src/queries/useDeleteRecord.ts
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from 'react-query';
 
-import { deleteRecord } from '@/apis';
+import { deleteRecord, IRecord } from '@/apis';
 
-export const useDeleteRecord = () => {
+export const useDeleteRecord = (recordId: IRecord['id']) => {
   const queryClient = useQueryClient();
 
   return useMutation(deleteRecord, {
     onSuccess: () => {
-      queryClient.resetQueries('record');
+      queryClient.resetQueries(['record', recordId]);
+    },
+    onError: () => {
+      alert('맥주 기록 삭제에 실패했어요.');
     },
   });
 };
