c28dc6537920191dca3fbef34af1016a7cbc8b84
cyjo9603 <cyjo9603@naver.com>
2022-06-22 23:10:19 +0900

    feat: ✨records 비어있을 때 empty 표시 적용

--- src/containers/RecordsContainer/RecordsContainer.tsx
+++ src/containers/RecordsContainer/RecordsContainer.tsx
@@ -3,6 +3,8 @@ import { NextPage, GetServerSideProps } from 'next';
 import styled from '@emotion/styled';
 import { useInView } from 'react-intersection-observer';
 
+import EmptyRecords from './EmptyRecords';
+
 import {
   IRecentlyVisitedCountry,
   getRecentlyVisitedCountry,
@@ -12,6 +14,7 @@ import {
 import BottomNavigation from '@/components/BottomNavigation';
 import RecordList from '@/components/RecordList';
 import { useGetMyRecords, useGetRecentlyVisitedCountry } from '@/queries';
+import Icon from '@/components/commons/Icon';
 
 interface RecordsContainerProps {
   myRecordResponse: IGetMyRecordsResponseData;
@@ -29,6 +32,13 @@ const StyledRecordsContainer = styled.section`
     & > .slim-weight {
       font-weight: 400;
     }
+  }
+
+  & > .empty-records-sad {
+    position: absolute;
+    bottom: 211px;
+    left: 50%;
+    transform: translateX(-50%);
   }
 `;
 
@@ -56,20 +66,29 @@ const RecordsContainer: NextPage<RecordsContainerProps> = ({
     triggerOnce: true,
   });
 
+  console.log(myRecords);
+
   return (
     <>
       <StyledRecordsContainer>
-        <h5 className="records-title">
-          <span className="slim-weight">{`최근에 `}</span>
-          <span>{recentlyVisitedCountry?.nameKor}</span>
-          <span className="slim-weight">{`에 다녀오셨군요!\n`}</span>
-          <span>{recentlyVisitedCountry?.nameKor}</span>
-          <span className="slim-weight">{` 여행 티켓은 총 `}</span>
-          <span>{`${recentlyVisitedCountry?.count}개`}</span>
-          <span className="slim-weight">{` 있어요.`}</span>
-        </h5>
-
-        <RecordList records={myRecords} lastItemRef={ref} />
+        {myRecords?.length ? (
+          <>
+            <h5 className="records-title">
+              <span className="slim-weight">{`최근에 `}</span>
+              <span>{recentlyVisitedCountry?.nameKor}</span>
+              <span className="slim-weight">{`에 다녀오셨군요!\n`}</span>
+              <span>{recentlyVisitedCountry?.nameKor}</span>
+              <span className="slim-weight">{` 여행 티켓은 총 `}</span>
+              <span>{`${recentlyVisitedCountry?.count}개`}</span>
+              <span className="slim-weight">{` 있어요.`}</span>
+            </h5>
+            <RecordList records={myRecords} lastItemRef={ref} />
+          </>
+        ) : (
+          <div className="empty-records-sad">
+            <EmptyRecords />
+          </div>
+        )}
       </StyledRecordsContainer>
       <BottomNavigation />
     </>
