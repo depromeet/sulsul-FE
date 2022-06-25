de8291be9a3a36023dbba54d308c7fc28b52af8f
hy57in <asst211118@tosspartners.com>
2022-06-26 03:25:47 +0900

    fix: :bug: 링크 공유 로직 변경

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -1,3 +1,5 @@
+import isServer from './isServer';
+
 import { openFailToast, openSuccessToast } from '@/utils/toast';
 
 interface Params {
@@ -8,37 +10,32 @@ interface Params {
 
 export async function share({ title, text, url = '' }: Params) {
   const messageToCopy = `[${title}]\n${text}\n${url}`.trim();
-  if (typeof window === 'undefined') {
-    return;
+  if (isServer()) {
+    return false;
   }
 
   try {
-    if (navigator.share) {
-      navigator
-        .share({ title, text, url })
-        .then(() => openSuccessToast({ message: '링크를 복사했어요.' }))
-        .catch((error) => {
-          openFailToast({ message: '링크를 복사하지 못했어요.' });
-          console.log(error);
-        });
-      return true;
-    }
-
-    throw new Error();
+    await window.navigator.share({ title, text, url });
   } catch (e) {
-    copyLink(messageToCopy);
+    // 아래로 이동
   }
+  copyLink(messageToCopy);
   return;
 }
 
 // NOTE: share API 미지원 브라우저에 대한 fallback
 export async function copyLink(link: string) {
+  if (isServer()) {
+    return false;
+  }
+
   try {
     navigator.clipboard.writeText(link);
     openSuccessToast({ message: '링크를 복사했어요.' });
-    throw new Error();
+
   } catch (e) {
     openFailToast({ message: '링크를 복사하지 못했어요.' });
+    console.log(e);
   }
 
   return false;
