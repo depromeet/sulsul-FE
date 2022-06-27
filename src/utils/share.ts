95c50a6a50b479f4147ce4828ffd99f345828a4a
hy57in <asst211118@tosspartners.com>
2022-06-27 15:15:14 +0900

    fix: :bug: share 로직 수정

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -1,6 +1,7 @@
 import isServer from './isServer';
+import { getOS } from './getOS';
 
-import { openFailToast, openSuccessToast } from '@/utils/toast';
+import { openSuccessToast } from '@/utils/toast';
 
 interface Params {
   title?: string;
@@ -8,35 +9,39 @@ interface Params {
   url?: string;
 }
 
+let isOpen = false;
+const DURATION = 2000;
+
 export async function share({ title, text, url = '' }: Params) {
-  const messageToCopy = `[${title}]\n${text}\n${url}`.trim();
   if (isServer()) {
-    return false;
+    return;
   }
 
-  try {
-    await window.navigator.share({ title, text, url });
-  } catch (e) {
-    // 아래로 이동
-  }
-  copyLink(messageToCopy);
-  return;
-}
+  const messageToCopy = `${title}\n${text}\n${url}`.trim();
 
-// NOTE: share API 미지원 브라우저에 대한 fallback
-export async function copyLink(link: string) {
-  if (isServer()) {
-    return false;
+  // NOTE: 안드로이드 카카오톡에서는 window.navigator.share가 안됨
+  if (window.navigator.userAgent.toLowerCase().includes('kakao') && getOS() === 'android') {
+    await window.navigator.clipboard.writeText(messageToCopy);
+    openSuccessToast({ message: '공유 링크가 복사 되었습니다.' });
+    return;
   }
 
-  try {
-    navigator.clipboard.writeText(link);
-    openSuccessToast({ message: '링크를 복사했어요.' });
-
-  } catch (e) {
-    openFailToast({ message: '링크를 복사하지 못했어요.' });
-    console.log(e);
+  if (getOS() === 'web') {
+    try {
+      if (isOpen) {
+        return;
+      }
+      await window.navigator.clipboard.writeText(messageToCopy);
+      openSuccessToast({ message: '공유 링크가 복사 되었습니다.' });
+      isOpen = true;
+      setTimeout(() => {
+        isOpen = false;
+      }, DURATION);
+      return;
+    } catch (e) {
+      return await window.navigator.share({ title, text, url });
+    }
   }
 
-  return false;
+  return await window.navigator.share({ title, text, url });
 }
