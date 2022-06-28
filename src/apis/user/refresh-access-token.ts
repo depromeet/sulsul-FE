93d23c871c666ea54b5225f0710d0f8f1c92dd47
cyjo9603 <cyjo9603@naver.com>
2022-06-29 01:17:11 +0900

    feat: :sparkles: getUser api ssr 기능 적용
    - ssr 에서 실패한 경우 accessToken 및 refreshToken 업데이트

--- src/apis/user/refresh-access-token.ts
+++ src/apis/user/refresh-access-token.ts
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 import { IBaseResponse } from '..';
 
+interface IRefreshAccessTokenResponse
+  extends IBaseResponse<{
+    accessToken: string;
+    responseToken: string;
+  }> {}
+
 /**
  * refreshToken으로 accessToken 갱신
  */
 export const refreshAccessToken = async () => {
   try {
-    const res = await axios.get<IBaseResponse<object>>('/api/v1/token/refresh');
+    const res = await axios.get<IRefreshAccessTokenResponse>('/api/v1/token/refresh');
     return res.data;
   } catch (error) {
     console.error(error);
