fa4831f171e7f059066820321f7ec254b280e72e
cyjo9603 <cyjo9603@naver.com>
2022-06-29 01:19:19 +0900

    fix: :bug: getInitialProps에서 axios interceptors memory leak 수정

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { refreshAccessToken } from '@/apis/user';
+
 const isServer = typeof window === 'undefined';
 
 export const BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
@@ -8,4 +10,27 @@ export const initAxiosConfig = () => {
   axios.defaults.baseURL = process.env.NODE_ENV === 'production' || isServer ? BASE_URL ?? '' : '';
   axios.defaults.timeout = 3000;
   axios.defaults.withCredentials = true;
+};
+
+export const initAxiosRefreshConfig = () => {
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const {
+        config,
+        response: { status },
+      } = error;
+
+      if (status === 401) {
+        const originalRequest = config;
+        // refreshToken으로 accessToken 갱신
+        await refreshAccessToken();
+        // 401로 요청 실패했던 요청 새로운 토큰으로 재요청
+        return axios(originalRequest);
+      }
+      return Promise.reject(error);
+    },
+  );
 };
