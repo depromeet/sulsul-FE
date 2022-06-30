f4b802fea0e7917af2ad264d2cc1c7b15c6d3b18
cyjo9603 <cyjo9603@naver.com>
2022-06-30 19:37:32 +0900

    fix: :bug: axios intercepter refresh logic 수정

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -13,24 +13,17 @@ export const initAxiosConfig = () => {
 };
 
 export const initAxiosRefreshConfig = () => {
-  axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    async (error) => {
-      const {
-        config,
-        response: { status },
-      } = error;
+  axios.interceptors.response.use(async (response) => {
+    const { config, data } = response;
 
-      if (status === 401) {
-        const originalRequest = config;
-        // refreshToken으로 accessToken 갱신
-        await refreshAccessToken();
-        // 401로 요청 실패했던 요청 새로운 토큰으로 재요청
-        return axios(originalRequest);
-      }
-      return Promise.reject(error);
-    },
-  );
+    if (data?.error?.code === 401) {
+      const originalRequest = config;
+      // refreshToken으로 accessToken 갱신
+      await refreshAccessToken();
+      // 401로 요청 실패했던 요청 새로운 토큰으로 재요청
+      return axios(originalRequest);
+    }
+
+    return response;
+  });
 };
