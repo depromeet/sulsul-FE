39a6b32ba06efcc79e8b414e6c3017a5cfbc9f82
hy57in <asst211118@tosspartners.com>
2022-07-01 02:25:52 +0900

    fix: removeCookies 테스트

--- src/containers/EtcContainer/EtcContainer.tsx
+++ src/containers/EtcContainer/EtcContainer.tsx
@@ -29,8 +29,8 @@ const EtcContainer = () => {
   const openLogoutModal = () => setIsLogoutModalOpen(true);
   const closeLogoutModal = () => {
     queryClient.clear();
-    removeCookies('accessToken');
-    removeCookies('refreshToken');
+    removeCookies('accessToken', { path: '/', domain: 'https://beerair.kr' });
+    removeCookies('refreshToken', { path: '/', domain: 'https://beerair.kr' });
     openSuccessToast({ message: '로그아웃 되었습니다.' });
     router.push(`${BASE_URL}/logout`);
   };
@@ -39,8 +39,8 @@ const EtcContainer = () => {
     setIsWithdrawalModalOpen(false);
     try {
       await deleteUserMutation();
-      removeCookies('accessToken');
-      removeCookies('refreshToken');
+      removeCookies('accessToken', { path: '/', domain: 'https://beerair.kr' });
+      removeCookies('refreshToken', { path: '/', domain: 'https://beerair.kr' });
       openSuccessToast({ message: '회원탈퇴 되었습니다.' });
       router.push(`${BASE_URL}`);
     } catch (error) {
