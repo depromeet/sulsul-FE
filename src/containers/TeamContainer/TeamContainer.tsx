217ded5b878d5db58596ef976469ff5149481076
HyojinKim <asst211118@tosspartners.com>
2022-07-02 09:08:14 +0900

    feat: :beers: 팀원 소개 페이지 꾸미기 (#222)
    * feat: :beers: 팀원 소개 페이지 꾸미기
    
    * feat: link tree로 변경

--- src/containers/TeamContainer/TeamContainer.tsx
+++ src/containers/TeamContainer/TeamContainer.tsx
@@ -8,10 +8,42 @@ const TeamContainer = () => {
     <StyledTeamContainer>
       <Header leftExtras={<BackButton />}>팀원 소개</Header>
       <img src="/images/beerair_member.png" width="100%" height="auto" alt="비어에어멤버" />
+      <section>
+        <h1 className="top-margin">
+          <a href="https://linktr.ee/beerair" target="_blank" rel="noopener noreferrer">
+            BeerAir가 더 궁금하시다면 클릭!
+          </a>
+        </h1>
+      </section>
     </StyledTeamContainer>
   );
 };
 
 export default TeamContainer;
 
-const StyledTeamContainer = styled.div``;
+const StyledTeamContainer = styled.div`
+  white-space: pre-line;
+  & > section {
+    padding: 0 20px;
+
+    & > h1 {
+      ${({ theme }) => theme.fonts.SubTitle1};
+    }
+
+    & > p {
+      ${({ theme }) => theme.fonts.Body1};
+      font-weight: 400;
+    }
+
+    & > a {
+      text-decoration: none;
+      ${({ theme }) => theme.fonts.Body1};
+      color: white;
+      text-align: center;
+    }
+
+    & > .top-margin {
+      margin-top: 20px;
+    }
+  }
+`;
